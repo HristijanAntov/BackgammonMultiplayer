1e5b03437b07e130744dfca6f9e25d75e847746e
HristijanAntov <hristijan_ninja@hotmail.com>
2021-06-23 20:19:53 +0200

    Applied some general game utils

---
+++ backend/backgammon/utils/index.ts
@@ -0,0 +1,14 @@
+import isEqual from "lodash/isEqual";
+
+import { GameState, PlayerType, Move } from "../types";
+
+import { INITIAL_STATE } from "../constants";
+
+export const getInitialState = (): GameState => ({ ...INITIAL_STATE });
+export const isFacingNorth = (player: PlayerType) => player === "W";
+
+export const getOpponent = (p: PlayerType): PlayerType =>
+  p === "W" ? "B" : "W";
+
+export const isEqualMove = (move: Move, anotherMove: Move) =>
+  isEqual(move, anotherMove);
