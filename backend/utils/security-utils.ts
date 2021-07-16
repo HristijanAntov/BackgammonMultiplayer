110f10f62260f68b307ad6e2a1616026fb65a6c6
HristijanAntov <hristijan_ninja@hotmail.com>
2021-07-16 20:22:36 +0200

    Added utils for auth handling

---
+++ backend/utils/security-utils.ts
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt";
+
+export async function hashPassword(password: string) {
+  const hashedResult = await bcrypt.hash(password, 10);
+  return hashedResult;
+}
+
+export async function doesPasswordMatchHash(hash: string, password: string) {
+  const isMatched = bcrypt.compare(password, hash);
+  return isMatched;
+}
