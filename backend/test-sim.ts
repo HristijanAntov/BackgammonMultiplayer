d4cf4ba525dbb860878591ccc310a48eff8fbc1f
HristijanAntov <hristijan_ninja@hotmail.com>
2021-07-16 20:21:07 +0200

    Just updates sim

--- backend/test-sim.ts
+++ backend/test-sim.ts
@@ -2,12 +2,21 @@ const printTree = require("print-tree");
 
 import Backgammon from "./backgammon/model";
 
-import { getPossibleMoveNodes } from "./backgammon/utils/move-generator";
-
 export function play() {
   const game = new Backgammon();
 
-  game.rollDice("W");
+  const p1 = game.rollDice("W");
+
+  game.move({ from: 12, to: 7, die: 5, isHit: false, player: "W" });
+  game.move({ from: 12, to: 7, die: 5, isHit: false, player: "W" });
+  console.log(game.getState(), "c");
+
+  game.undo();
+  game.undo();
+
+  console.log(game.getState(), "after undo");
+
+  // console.log(game.getState(), "after undo");
 
   // printTree(
   //   { children: movesNodes },
