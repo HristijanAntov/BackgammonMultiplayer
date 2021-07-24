b11264e78af68cfa098e3a47e1b14f6710974e1e
HristijanAntov <hristijan_ninja@hotmail.com>
2021-07-24 10:27:45 +0200

    Removed a couple of utils not used anymore, added a util to construct an IError entity

--- backend/utils/index.ts
+++ backend/utils/index.ts
@@ -1,7 +1,8 @@
-export const getUniqueId = () => "_" + Math.random().toString(36).substr(2, 9);
-export const wait = (ms: number) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
+import { IError, ErrorType } from "../networking/types";
 
-//copied this from here: https://decipher.dev/30-seconds-of-typescript/docs/powerset/
-export const powerSet = (arr: any[]) =>
-  arr.reduce((a, v) => a.concat(a.map((r: any) => [v].concat(r))), [[]]);
+export const getUniqueId = () => "_" + Math.random().toString(36).substr(2, 9);
+
+export const generateError = (errorType: ErrorType, payload: any): IError => ({
+  errorType,
+  payload,
+});
